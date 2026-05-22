cd76cb9ede2cf395dea0a9e5c517330672526309
cynjean <cynthia.2056@yahoo.com>
2026-05-22 02:50:02 -0500

    Fetch and display astronauts from API

--- index.js
+++ index.js
@@ -5,9 +5,27 @@
 // Hint: Select the <ul> element with the id "astronaut-list"
 // Hint: Loop through the data.people array and create <li> elements for each astronaut's name
 // Hint: Append the <li> elements to the <ul> element
+function displayAstronauts(data) {
+    const astronautList = document.getElementById("astronaut-list");     
 
+    data.people.forEach((person) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = person.name;
+    astronautList.appendChild(listItem);
+    })
+}
 // Fetch data from the external API
-
+fetch("http://api.open-notify.org/astros.json")
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    displayAstronauts(data);
+    console.log(data)
+})
+.catch(function(error) {
+    console.error("Error fetching data:", error);
+})
 // Hint: Use fetch() to make an HTTP request to "http://api.open-notify.org/astros.json"
 // Hint: Use .then() to process the response and convert it to JSON
 // Hint: Call the displayAstronauts() function with the fetched data
